c1ea418b093ec07286c0b41ecebd4819fe20b82d
Nabil Furqan <73362168+nabilfurqan@users.noreply.github.com>
2026-09-09 15:51:41 +0700

    Add disconnect endpoint

---
+++ api/disconnect.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__ . '/../lib/cloudflare.php';
+bootstrap_session();
+require_csrf();
+
+$_SESSION = [];
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'] ?? '', (bool) $params['secure'], (bool) $params['httponly']);
+}
+session_destroy();
+json_response(['success' => true]);
